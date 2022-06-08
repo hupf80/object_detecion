d9f109c682b4e6c6a737ac1f8485f512e3f0f0bc
hupf80 <57186905+hupf80@users.noreply.github.com>
2022-06-08 20:56:30 +0200

    Update object_detection.cpp

--- src/object_detection.cpp
+++ src/object_detection.cpp
@@ -17,7 +17,7 @@
 #include <sensor_msgs/Image.h>
 #include <camera_info_manager/camera_info_manager.h>
 #include <image_transport/image_transport.h>
-#include <image_geometry/pinhole_camera_model.h>
+
 
 #include <cv_bridge/cv_bridge.h>
 #include <ros/package.h>
