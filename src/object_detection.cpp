478ec2460c3eb5b08140dce854241348c8762a1e
hupf80 <hupf80@gmx.de>
2022-06-13 17:48:35 +0200

    Added nearest neighbour tracking method.

--- src/object_detection.cpp
+++ src/object_detection.cpp
@@ -7,10 +7,15 @@
 #include <memory>
 #include <string>
 #include <map>
+
+
+#include<math.h>
+
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc_c.h>
 
-
+#include <algorithm>
+#include <vector>
 #include <ros/ros.h>
 
 #include <sensor_msgs/image_encodings.h>
@@ -53,20 +58,39 @@ public:
     std::string pub_bbximg_topic_name;
     std::string classes_yaml_path;
     double conv_tsh_val;
+    int tracking_closest_tsh_pixel;
+    int MaxObjAge;
     int y_detection_border;
 
+    struct CenterPoint{
+        float CpX;
+        float CpY;
+    };
 
+    struct DistId{
+        double Dist;
+        int ObjIdFrom;
+        int ObjIdTo;
+    };
 
 
     struct Object{
+        int DictID;
         int ClassSpecID;
+        int Age;
         std::string ClassName;
         float Prob;
         int RectXmin;
         int RectYmin;
         int RectHeight;
         int RectWidth;
+        CenterPoint Center;
     };
+
+    typedef std::pair<int, DistId> DistDictType;
+
+
+    int ObjCnt = 0;
 
     std::map<int, Object> ObjectDict;
     
@@ -78,6 +102,8 @@ public:
         std::string const package_path = ros::package::getPath("object_detection");
         
         ros::param::get("/conv_tsh", conv_tsh_val);
+        ros::param::get("/tracking_closest_tsh_pixel", tracking_closest_tsh_pixel);
+        ros::param::get("/tracking_max_obj_age", MaxObjAge);
         ros::param::get("/TopicNamePubImgRaw", pub_bbximg_topic_name);
         ros::param::get("/TopicNameSubImgRaw", sub_img_topic_name);
         ros::param::get("/y_detect_border", y_detection_border);
@@ -135,11 +161,13 @@ public:
 
     void imageCallback(const sensor_msgs::ImageConstPtr& image_msg, const sensor_msgs::CameraInfoConstPtr& info_msg)
     {
-        
+            
         cv::Mat image;
         cv_bridge::CvImagePtr input_bridge;
         bounding_box_msgs::boundingbox bbx;
-
+        bounding_box_msgs::boundingboxes bbxs;
+        std_msgs::Header h = image_msg->header;
+        bbxs.header = h;
         try {
             input_bridge = cv_bridge::toCvCopy(image_msg, sensor_msgs::image_encodings::BGR8);
 
@@ -155,7 +183,7 @@ public:
         
         //prepare batch data
         std::vector<cv::Mat> batch_img;
-	
+
         std::vector<BatchResult> batch_res;
         
         cv::Size s = image.size();
@@ -169,94 +197,253 @@ public:
         objdetector->detect(batch_img, batch_res);
         
         //draw Boundingboxes which are in the detecion area
-	    for (const auto &r : batch_res[0])
-	    {
-	    	cv::Point center_of_rect = (r.rect.br() + r.rect.tl())*0.5;
 
-		    if(center_of_rect.y >= y_detection_border)
-		    {
-            bbx.Class=names[r.id];
-            bbx.probability=r.prob;
-            bbx.rect_xmin=r.rect.x ;
-            bbx.rect_ymin=r.rect.y ;
-            bbx.rect_height=r.rect.height;
-            bbx.rect_width=r.rect.width;
+        std::map<int, Object> TmpObjDict;
+
+        for (const auto &r : batch_res[0])
+        {
+            cv::Point center_of_rect = (r.rect.br() + r.rect.tl())*0.5;
+
+            if(center_of_rect.y >= y_detection_border)
+            {
+
+                Object Tmp;
+                Tmp.ClassName = names[r.id];
+                Tmp.DictID = ObjCnt;
+                Tmp.Age = 0;
+                Tmp.Prob=r.prob;
+                Tmp.RectXmin=r.rect.x ;
+                Tmp.RectYmin=r.rect.y ;
+                Tmp.RectHeight=r.rect.height;
+                Tmp.RectWidth=r.rect.width;
+                Tmp.Center.CpX = r.rect.x + (r.rect.width/2);
+                Tmp.Center.CpY = r.rect.y + (r.rect.height/2);
+
+                TmpObjDict.insert ( std::pair<int,Object>(ObjCnt,Tmp) );
+
+                ObjCnt++;
+            }
+
+        }
+
+        if(!ObjectDict.empty())
+        {
+            std::cout << "Current Emelents " << ObjectDict.size() << '\n';
+            std::vector<int> MachedIdsTo;
+            std::vector<int> MachedIdsFrom;
+            if (!TmpObjDict.empty()){
+
+                for (auto const& l : TmpObjDict){
+                    
+                    std::cout << "Temp Detected Emelents " << TmpObjDict.size() << '\n';
+                    Object TmpObj = l.second;
+                    int tmpid = l.first;
+
+                    std::map<int, DistId> DistDict;     //In DistDict the ID (key) is the Same ID as in the TmpObjDict
+
+                    for (auto const& x : ObjectDict)    //Get all distances of the same class to each other
+                    {   
+                        int Id = x.first;
+                        Object Obj = x.second;
+                        
+                        if(Obj.ClassName == TmpObj.ClassName) //Object is same Class
+                        {
+                            DistDictType DistElm;
+                            DistElm.first = tmpid;
+                            DistElm.second.ObjIdFrom = tmpid;
+                            DistElm.second.ObjIdTo = Id;
+                            DistElm.second.Dist = EuklideanDist(TmpObj,Obj);
+                            
+                            DistDict.insert (std::pair<int,DistId>(tmpid,DistElm.second));
+
+
+                        }
+                    }
+
+                    if(!DistDict.empty()){
+
+                        DistDictType clst = GetClstId(DistDict);
+
+                        DistId  temp = clst.second;
+                        
+                        if ((int)clst.second.Dist < tracking_closest_tsh_pixel){
+
+                            ObjectDict[clst.second.ObjIdTo] = TmpObjDict[clst.second.ObjIdFrom];
+                            Object MatchObj = ObjectDict[clst.second.ObjIdTo];
+
+                            MachedIdsFrom.push_back(clst.second.ObjIdFrom);
+
+                            MachedIdsTo.push_back(clst.second.ObjIdTo);
+
+                            if (DistDict.count(clst.second.ObjIdFrom) > 0) {
+                                
+                                DistDict.erase(clst.second.ObjIdFrom);
+
+                            }
+
+                        }
+                    }
+                    else{       //IF there is nothing in the current object which is the same class, add it!
+
+                        ObjectDict.insert(std::pair<int,Object>(ObjCnt,TmpObj) );
+
+
+                    }
+                }
+
+                for(auto it = std::begin(MachedIdsFrom); it != std::end(MachedIdsFrom); ++it) {
+                    if (TmpObjDict.count(*it) > 0) {
+                        TmpObjDict.erase(*it);
+                    }
+                }
+
             
-            //std::cout << " id:" << r.id << " prob:" << r.prob << " rect:" << r.rect << std::endl;
+            std::cout << "Aftermatching Temp Elements " << TmpObjDict.size() << '\n';
+            std::cout << "Aftermatching Object Elements " << ObjectDict.size() << '\n';
+
+            }
             
-            cv::rectangle(image, r.rect, cv::Scalar(255, 0, 0), 2);
+            if(!TmpObjDict.empty()){
+                for (auto const& x : TmpObjDict){
+
+                    if(x.second.Center.CpY >1000){ //It seems the object has left the Window in y-direction
+                        if (TmpObjDict.count(x.first) > 0) {
+                        TmpObjDict.erase(x.first);
+                        }
+                        
+
+                    }
+                    else{
+
+                        ObjectDict.insert ( std::pair<int,Object>(x.first,x.second) );
+
+                    }
+
+                    
+
+                }
+            }
+            //Get all unmatched objects in the dict:
+            std::vector<int> UnmatchedIds;
+            std::vector<int> ToRemoveObjs;
+            for(auto const& x : ObjectDict){
+
+                int item = x.first;
+
+                if ( std::find(MachedIdsTo.begin(), MachedIdsTo.end(), item) != MachedIdsTo.end() ){
+                    //Do that                
+                }
+                else{
+                    if (x.second.Age >= MaxObjAge)
+                    {
+                        ToRemoveObjs.push_back(item);
+                    }
+                    else{
+                        Object temp = x.second;
+                        temp.Age++;
+                        ObjectDict[item] = temp;
+                    } 
+                }
+  
+            }
+
+            for(auto it = std::begin(ToRemoveObjs); it != std::end(ToRemoveObjs); ++it) {
+
+                if (ObjectDict.count(*it) > 0) {
+                    ObjectDict.erase(*it);
+                }
+            }
+
+            
+
+
+            for(auto it = std::begin(MachedIdsTo); it != std::end(MachedIdsTo); ++it) {
+
+                if (TmpObjDict.count(*it) > 0) {
+
+                    TmpObjDict.erase(*it);
+                }
+            }
+
+        }
+
+        else{
+
+            if(!TmpObjDict.empty()){
+
+                for (auto const& x : TmpObjDict){
+
+                    ObjectDict.insert ( std::pair<int,Object>(x.first,x.second) );
+
+                }
+
+            }
+
+        }
+
+        
+        for (auto const& h : ObjectDict){
+
+            Object Obj = h.second;
+            int id = h.first;
+                
+            bbx.Class=Obj.ClassName;
+            bbx.probability=Obj.Prob;
+            bbx.rect_xmin=Obj.RectXmin;
+            bbx.rect_ymin=Obj.RectYmin;
+            bbx.rect_height=Obj.RectHeight;
+            bbx.rect_width=Obj.RectWidth;
+            
+            cv::Rect rect1;
+            rect1.x = Obj.RectXmin;
+            rect1.y = Obj.RectYmin;
+            rect1.width = Obj.RectWidth;
+            rect1.height = Obj.RectHeight;
+            
+            
+            cv::rectangle(image, rect1, cv::Scalar(255, 0, 0), 2);
             
             std::stringstream stream;
             
-            stream << std::fixed << std::setprecision(2)  << "  score:" << r.prob <<  " "<< names[r.id];
+            stream << std::fixed << std::setprecision(2)  << "  score:" << Obj.Prob <<  " "<< Obj.ClassName << "id:" << id;
             
-            cv::putText(image, stream.str(), cv::Point(r.rect.x, r.rect.y - 5), 0, 1, cv::Scalar(0, 0, 255), 2);
-            
+            cv::putText(image, stream.str(), cv::Point(Obj.RectXmin, Obj.RectYmin - 5), 0, 1, cv::Scalar(0, 0, 255), 2);
+
             boundingbx_pub_.publish(bbx);
+            bbxs.bounding_boxes.push_back(bbx);
             }
-        }
 
-        pub_.publish(input_bridge->toImageMsg());
+            boundingbxs_pub_.publish(bbxs);
+            pub_.publish(input_bridge->toImageMsg());
 
-        
-    }
-
-    void imagedepthCallback(const sensor_msgs::ImageConstPtr& image_msg, const sensor_msgs::CameraInfoConstPtr& info_msg)
-    {
-        
-        cv::Mat image;
-        cv_bridge::CvImagePtr input_bridge;
-
-        bounding_box_msgs::boundingbox bbx;
-
-        try {
-            input_bridge = cv_bridge::toCvCopy(image_msg, sensor_msgs::image_encodings::BGR8);
-            image = input_bridge->image;
             
-
-        }
-        catch (cv_bridge::Exception& ex){
-            ROS_ERROR("[object_detection] Failed to convert image");
-            return;
-        }
-        
-        //cam_model_.fromCameraInfo(info_msg);
-
-        //prepare batch data
-        std::vector<cv::Mat> batch_img;
-        std::vector<BatchResult> batch_res;
-        cv::Mat temp0 = image.clone();
-        batch_img.push_back(temp0);
-        //Timer timer;
-        //do detection process
-        //timer.reset();
-        objdetector->detect(batch_img, batch_res);
-        //timer.out("detect");
-
-        //disp
-        for (int i=0;i<batch_img.size();++i)
-        {            
-            for (const auto &r : batch_res[i])
-            {
-                
-                bbx.Class=r.id;
-                bbx.probability=r.prob;
-                bbx.rect_xmin=r.rect.x ;
-                bbx.rect_ymin=r.rect.y ;
-                bbx.rect_height=r.rect.height;
-                bbx.rect_width=r.rect.width;
-                std::cout <<"batch "<<i<< " id:" << r.id << " prob:" << r.prob << " rect:" << r.rect << std::endl;
-                cv::rectangle(batch_img[i], r.rect, cv::Scalar(255, 0, 0), 2);
-                std::stringstream stream;
-                stream << std::fixed << std::setprecision(2) << "id:" << r.id << "  score:" << r.prob;
-                cv::putText(batch_img[i], stream.str(), cv::Point(r.rect.x, r.rect.y - 5), 0, 0.5, cv::Scalar(0, 0, 255), 2);
-                boundingbx_pub_.publish(bbx);
-            }
-
-        pub_.publish(input_bridge->toImageMsg());
-        }
-        
     }
+
+    double EuklideanDist(Object Obj1, Object Obj2)
+    {
+
+        double dist = sqrt(pow((Obj1.Center.CpX - Obj2.Center.CpX), 2) + pow((Obj1.Center.CpY - Obj2.Center.CpY), 2));      
+
+        return dist;
+    }
+
+
+
+
+    struct CompareSecond
+    {
+        bool operator()(const DistDictType& left, const DistDictType& right) const
+        {
+            return left.second.Dist < right.second.Dist;
+        }
+    };
+
+    DistDictType GetClstId(std::map<int, DistId> DistDict) 
+    {
+    std::pair<int,DistId > min = *min_element(DistDict.begin(), DistDict.end(), CompareSecond());
+    return min; 
+    }
+
 };
 
 
